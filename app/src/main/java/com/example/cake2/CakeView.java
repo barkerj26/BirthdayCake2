9fb0de819665a7f693a4a701f332ac29b3f4a636
barkerj26 <barkerj26@up.edu>
2023-08-31 16:31:33 -0700

    Add frosting switch and an extra candle

--- app/src/main/java/com/example/cake2/CakeView.java
+++ app/src/main/java/com/example/cake2/CakeView.java
@@ -117,8 +117,9 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth / 2 - candleWidth / 2, cakeTop);
+        //Now candles to the left and right
+        drawCandle(canvas, cakeLeft + cakeWidth / 3 - candleWidth / 2, cakeTop);
+        drawCandle(canvas, cakeLeft + (cakeWidth / 3) * 2 - candleWidth / 2, cakeTop);
 
     }//onDraw
 
